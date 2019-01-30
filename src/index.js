7a1d0a40df9b33567a127fd4ef410a5bf276c4ef
Jo Minji <mingi2687@gamil.com>
2019-01-30 22:22:58 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,119 @@
+let boardState = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+//처음 오목판 상태
+
+function drawBoard() {
+  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+      if (boardState[rowIndex][colIndex] === 1) {
+        colEl.classList.add("checked");
+      } else {
+        colEl.classList.remove("checked");
+      }
+    });
+  });
+
+  if (bingo(boardState)) {
+    document.querySelector(".result").textContent = "빙고!";
+    document.querySelector(".reset").classList.add("show");
+  }
+}
+
+function resetBoard() {
+  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+      colEl.classList.remove("checked");
+    });
+  });
+  document.querySelector(".result").textContent = "";
+  document.querySelector(".reset").classList.remove("show");
+}
+
+function bingo(arr) {
+  // 가로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[i][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  // 세로줄 확인 (루프)
+  for (let i = 0; i < 5; i++) {
+    // '이제까지 본 것이 전부 x표시가 되어있다'
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][i] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    // 대각선 확인 (루프)
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  {
+    // 반대쪽 대각선 확인 (루프)
+    let checked = true;
+    for (let j = 0; j < 5; j++) {
+      if (arr[j][4 - j] === 0) {
+        checked = false;
+      }
+    }
+    if (checked) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+  rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+    colEl.addEventListener("click", e => {
+      if (!bingo(boardState)) {
+        boardState[rowIndex][colIndex] = 1;
+        drawBoard();
+      }
+    });
+  });
+});
+
+document.querySelector(".reset").addEventListener("click", e => {
+  // alert('asdf')
+  boardState = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+  resetBoard();
+});
+
+drawBoard();
